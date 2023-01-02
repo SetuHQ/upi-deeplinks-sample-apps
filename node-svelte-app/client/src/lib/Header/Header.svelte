47d1c650de69e1d08c4f6ce07e17e8c056349fea
Avinash Prajapati <avinashprajapati914@gmail.com>
2023-01-02 19:11:52 +0530

    feat: 🎸 add svelte app

---
+++ node-svelte-app/client/src/lib/Header/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Icon from "../../assets/icon.svg";
+</script>
+
+<header>
+  <nav class="navbar flex-nav">
+    <div class="nav-left flex-al-center">
+      <img alt="nav-logo" class="logo" src={Icon} />
+      <a href="/" class="nav-logo"> GoFIN </a>
+    </div>
+  </nav>
+</header>
+
+<style>
+  header {
+    position: sticky;
+    top: 0rem;
+    z-index: 100;
+  }
+  .navbar {
+    background-color: var(--clr-white);
+    height: 8rem;
+    border-bottom: 1px solid var(--clr-grey-15);
+    padding: 2rem 3rem;
+    box-shadow: var(--lighter-shadow);
+  }
+  @media only screen and (max-width: 42.375em) {
+    .navbar {
+      padding: 2rem 1rem;
+    }
+  }
+  .navbar .nav-logo {
+    font-size: 2.2rem;
+    font-weight: 700;
+    margin-left: 1rem;
+  }
+</style>
---
+++ node-svelte-app/server/index.ts
@@ -0,0 +1,81 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { SetuUPIDeepLink } from "@setu/upi-deep-links";
+import "dotenv/config.js";
+
+const whitelist: string[] = [];
+const port = process.env.PORT || 4000;
+const corsOptions = {
+  origin: whitelist,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: true, credentials: true }));
+} else {
+  app.use(cors(corsOptions));
+}
+app.get("/", (req, res) => {
+  res.json({ msg: "HELLO FROM AVINASH" });
+});
+const schemeID = process.env.SCHEME_ID || "";
+const secret = process.env.SECRET || "";
+const productID = process.env.PRODUCT_ID || "";
+app.post("/upi-pay", async (req, res) => {
+  const { price, id } = req.body;
+  const upidl = SetuUPIDeepLink({
+    schemeID,
+    secret,
+    productInstanceID: productID,
+    mode: "SANDBOX",
+    authType: "JWT",
+  });
+  try {
+    const paymentLinkResponse = await upidl.createPaymentLink({
+      amountValue: price * 100,
+      billerBillID: id,
+      amountExactness: "EXACT",
+    });
+    res.status(200).json({ paymentLinkResponse });
+  } catch (error) {
+    res.status(400).json({
+      message: "Some Problems with UPI",
+      error,
+    });
+  }
+});
+app.post("/mock-payment", async (req, res) => {
+  const { response, price } = req.body;
+  const upidl = SetuUPIDeepLink({
+    schemeID,
+    secret,
+    productInstanceID: productID,
+    mode: "SANDBOX",
+    authType: "JWT",
+  });
+  try {
+    const data = await upidl.triggerMockPayment({
+      amountValue: price * 100,
+      vpa: response.paymentLink.upiID,
+      platformBillID: response.platformBillID,
+    });
+    res.status(200).json({ data });
+  } catch (error) {
+    res.status(400).json({
+      message: "Some Problems with UPI",
+      error,
+    });
+  }
+});
+
+app.use((_, res, _2) => {
+  res.status(404).json({ error: "NOT FOUND" });
+});
+
+app.listen(port, () => {
+  console.log("server started on port " + port);
+});
